90b715012aa1513e99a3dc50a6a2d5f0231f04ac
Pavel Aksuyitn <thxu2@ya.ru>
2020-09-22 21:59:09 +0300

    Add sample logic

---
+++ SampleApi/Migrations/20200922184322_Init.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SampleApi.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoLists",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Owner = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoLists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TodoListItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    TodoListId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoListItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TodoListItems_TodoLists_TodoListId",
+                        column: x => x.TodoListId,
+                        principalTable: "TodoLists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoListItems_TodoListId",
+                table: "TodoListItems",
+                column: "TodoListId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoListItems");
+
+            migrationBuilder.DropTable(
+                name: "TodoLists");
+        }
+    }
+}
